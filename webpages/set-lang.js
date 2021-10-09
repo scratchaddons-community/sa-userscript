bcb9c6583c2529ab6c8776d4cdc7abf1c9644b29
RedGuy12 <paul@reid-family.org>
2021-10-09 11:48:04 -0500

    set-lang.js as well

--- webpages/set-lang.js
+++ webpages/set-lang.js
@@ -1,4 +1,5 @@
 import getDirection from "./rtl-list.js";
+import chrome from "../libraries/common/chrome.js";
 
 document.documentElement.lang = chrome.i18n.getUILanguage();
 document.body.dir = getDirection(chrome.i18n.getUILanguage());
