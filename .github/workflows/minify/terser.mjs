2c57596a14d22aea82dcd9bd630bcea0ae62eb50
RedGuy12 <paul@reid-family.org>
2021-12-02 16:30:17 -0600

    no minify

--- .github/workflows/minify/terser.mjs
+++ .github/workflows/minify/terser.mjs
@@ -7,8 +7,17 @@ import getInDir from "./getInDir.mjs";
 /* Javascript */
 const terserConfig = JSON.parse(readFileSync(resolve(process.cwd(), "./.terserrc")));
 
-getInDir({ ext: ".js" }).forEach(async (filePath) => {
-  if (filePath.endsWith(".user.js")) return;
+const NO_MINIFY = [
+  "/content-scripts/load-redux.js",
+  "/content-scripts/prototype-handler.js",
+  "/content-scripts/fix-console.js",
+  "/libraries/common/cs/text-color.js",
+  ".user.js"
+]
+
+getInDir({ext: ".js"}).forEach(async (filePath) => {
+
+  if (NO_MINIFY.some(pattern=>filePath.endsWith(pattern)))return
   const source = await readFile(filePath, "utf8").catch(console.error);
   const minfied = (await minifyJs(source, terserConfig).catch(e=>console.log(filePath)).catch(console.error)).code;
   await writeFile(filePath, minfied);
