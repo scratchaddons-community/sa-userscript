165b9abd7c128728b8a8b74bd27bf3dda917dfb5
RedGuy12 <paul@reid-family.org>
2022-10-18 00:24:47 -0500

    %addon-self-dir% will be annoying later
    Signed-off-by: RedGuy12 <paul@reid-family.org>

--- postcss.config.js
+++ postcss.config.js
@@ -3,9 +3,5 @@ const cssNanoConfig = { preset: ["default", { mergeIdents: true, svgo: false }] 
 module.exports = {
   cssNanoConfig,
   map: false,
-  plugins: [
-    require("postcss-import"),
-    require("cssnano")(cssNanoConfig),
-    require("autoprefixer")({ grid: "autoplace" }),
-  ],
+  plugins: [require("cssnano")(cssNanoConfig), require("autoprefixer")({ grid: "autoplace" })],
 };
