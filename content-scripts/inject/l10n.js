7f9c94b7bed47add48f00c5dbdd83f78ba655eb0
RedGuy12 <paul@reid-family.org>
2022-10-20 21:50:15 -0500

    actually use the bundled addons-l10n
    Signed-off-by: RedGuy12 <paul@reid-family.org>

--- content-scripts/inject/l10n.js
+++ content-scripts/inject/l10n.js
@@ -8,29 +8,24 @@ export default class UserscriptLocalizationProvider extends LocalizationProvider
   }
 
   async loadByAddonId(addonId) {
-    if (addonId !== "_general" && !this.generalLoaded) {
-      await this.loadByAddonId("_general");
+    if (this.generalLoaded) {
+      return;
     }
     let addonMessages = {};
     for (const dir of this._urls) {
       let resp;
       let messages = {};
-      const url = `${dir}/${addonId}.json`;
+      const url = `${dir}.json`;
       try {
         resp = await fetch(url);
         messages = await resp.json();
       } catch (_) {
-        if (addonId === "_general") {
-          this._urls.delete(dir);
-        }
         continue;
       }
       addonMessages = Object.assign(messages, addonMessages);
       this.messages = Object.assign(messages, this.messages);
     }
-    if (addonId === "_general") {
-      this._reconfigure();
-      this.generalLoaded = true;
-    }
+    this._reconfigure();
+    this.generalLoaded = true;
   }
 }
